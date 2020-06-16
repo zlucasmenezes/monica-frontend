7977123a143eb3a9f423630ef4d8150d9c016cd1
Lucas <zlucasmenezes@gmail.com>
2020-06-16 19:55:29 -0300

    page container component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { SharedModule } from '../../shared/shared.module';
 import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PageContainerComponent } from './components/page-container/page-container.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageContainerComponent
   ],
   imports: [
     SharedModule
