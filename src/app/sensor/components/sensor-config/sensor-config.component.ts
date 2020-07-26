cd2a3eea5633ba32629cfceb6e2b20a5aea79103
Lucas <zlucasmenezes@gmail.com>
2020-07-26 13:07:42 -0300

    sensor configuration

--- src/app/sensor/components/sensor-config/sensor-config.component.ts
+++ src/app/sensor/components/sensor-config/sensor-config.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Form } from '@angular/forms';
-import { ISensorConfig, ISensorType } from '../../sensor.model';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ISensorType } from '../../sensor.model';
+import formUtils from 'src/app/shared/utils/form-utils';
 
 @Component({
   selector: 'm-sensor-config',
@@ -12,8 +13,11 @@ export class SensorConfigComponent implements OnInit {
   @Input() public form: FormGroup;
 
   @Input() set configuration(sensorType: ISensorType) {
+    this.sensorType = sensorType;
     this.initForm(sensorType);
   }
+
+  public sensorType: ISensorType;
 
   constructor(private fb: FormBuilder) { }
 
@@ -25,11 +29,19 @@ export class SensorConfigComponent implements OnInit {
     sensorType.config.forEach(config => {
       (this.form.get('config') as FormArray).push(
         this.fb.group({
-          parameter: [config.parameter],
-          value: [config.default]
+          parameter: [config.parameter, Validators.required],
+          value: [config.default, Validators.required]
         })
       );
     });
+  }
+
+  public getFormArray(): FormArray {
+    return (this.form.get('config') as FormArray);
+  }
+
+  public getError(index: number, control: string): string {
+    return formUtils.getError(this.getFormArray().at(index) as FormGroup, control);
   }
 
 }
