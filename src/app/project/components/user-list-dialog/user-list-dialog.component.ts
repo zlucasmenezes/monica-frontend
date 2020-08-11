22f0033659bbaa39c582c7f59fb07c220b222878
Lucas <zlucasmenezes@gmail.com>
2020-08-10 21:15:28 -0300

    filter user list

--- src/app/project/components/user-list-dialog/user-list-dialog.component.ts
+++ src/app/project/components/user-list-dialog/user-list-dialog.component.ts
@@ -3,6 +3,9 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { IUser } from 'src/app/auth/auth.model';
 import arrayUtils from 'src/app/shared/utils/array-utils';
+import { ReplaySubject, Subject } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'm-user-list-dialog',
@@ -13,12 +16,23 @@ export class UserListDialogComponent implements OnInit {
 
   public users: IUser[];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: IUser[]) {
-    console.log(this.data);
-  }
+  public usersFiltered$: ReplaySubject<IUser[]> = new ReplaySubject<IUser[]>(1);
+  public usersFilter = new FormControl();
+
+  private onDestroy: Subject<void> = new Subject<void>();
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: IUser[]) { }
 
   ngOnInit() {
-    this.users = arrayUtils.orderBy(this.data, 'ASC', 'firstName');
+    this.usersFiltered$.next(this.users = arrayUtils.orderBy(this.data, 'ASC', 'firstName'));
+    this.subscribeForm();
+  }
+
+  private async subscribeForm() {
+    this.usersFilter.valueChanges.pipe(takeUntil(this.onDestroy))
+    .subscribe((value: string) => {
+      this.filterUsers(value);
+    });
   }
 
   public getInitials(user: IUser): string {
@@ -26,6 +40,11 @@ export class UserListDialogComponent implements OnInit {
     const lastName = names[names.length - 1];
 
     return `${user.firstName[0]}${lastName[0]}`;
+  }
+
+  public filterUsers(filter: string) {
+    const fields = ['fullName', 'username', 'email'];
+    this.usersFiltered$.next(arrayUtils.filter(this.users, filter, fields));
   }
 
 }
