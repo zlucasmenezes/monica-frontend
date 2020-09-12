42b4bc32265b91f0cd86df0a6ff3a2849117af9b
Lucas <zlucasmenezes@gmail.com>
2020-09-12 14:39:55 -0300

    filter sensors

--- src/app/thing/pages/thing-details/thing-details.component.ts
+++ src/app/thing/pages/thing-details/thing-details.component.ts
@@ -4,13 +4,15 @@ import { ISensorPopulated } from './../../../sensor/sensor.model';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IProjectPopulated } from 'src/app/project/project.model';
 import { IThingPopulated } from '../../thing.model';
-import { Subject } from 'rxjs';
+import { ReplaySubject, Subject } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ThingService } from '../../thing.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ProjectService } from 'src/app/project/project.service';
 import { ICardMenuItem } from 'src/app/shared/components/card/card.model';
 import arrayUtils from 'src/app/shared/utils/array-utils';
+import { FormControl } from '@angular/forms';
+import { takeUntil, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'm-thing-details',
@@ -23,7 +25,9 @@ export class ThingDetailsComponent implements OnInit, OnDestroy {
   public thing: IThingPopulated;
 
   public sensors: ISensorPopulated[];
-  public sensorsMenuItems: ICardMenuItem[];
+  public sensorsFiltered$: ReplaySubject<ISensorPopulated[]> = new ReplaySubject<ISensorPopulated[]>(1);
+  public sensorsMenuItems: ReplaySubject<ICardMenuItem[]> = new ReplaySubject<ICardMenuItem[]>(1);
+  public sensorsFilter = new FormControl();
 
   private onDestroy: Subject<void> = new Subject<void>();
 
@@ -35,14 +39,25 @@ export class ThingDetailsComponent implements OnInit, OnDestroy {
     private projectService: ProjectService,
     private sensorService: SensorService,
     private socketIOService: SocketIOService
-  ) { }
+  ) {
+    this.socketIOService.join(`thing:${this.getThingId()}`);
+  }
 
   async ngOnInit(): Promise<void> {
     this.project = await this.getProject();
     this.thing = await this.getThing();
-    this.sensors = arrayUtils.orderBy(await this.getSensors(), 'ASC', 'name');
-    this.sensorsMenuItems = this.getSensorsMenuItems();
-    this.socketIOService.join(`thing:${this.getThingId()}`);
+
+    this.sensorsFiltered$.next(this.sensors = arrayUtils.orderBy(await this.getSensors(), 'ASC', 'name'));
+    this.subscribeForm();
+
+    this.subscribeSensorsMenuItems();
+  }
+
+  private subscribeForm(): void {
+    this.sensorsFilter.valueChanges.pipe(takeUntil(this.onDestroy))
+    .subscribe((value: string) => {
+      this.filterSensors(value);
+    });
   }
 
   private getProjectId(): string {
@@ -65,27 +80,29 @@ export class ThingDetailsComponent implements OnInit, OnDestroy {
     return await this.sensorService.getSensors(this.getProjectId(), this.getThingId());
   }
 
-  public getSensorsMenuItems(): ICardMenuItem[] {
-    if (!this.sensors) { return []; }
+  public subscribeSensorsMenuItems(): void {
+    this.sensorsFiltered$.pipe(takeUntil(this.onDestroy))
+    .subscribe((sensorsFiltered) => {
+      const sensors = {};
 
-    const sensors = {};
-    this.sensors.forEach(s => {
-      if (!sensors[s.type.type]) { sensors[s.type.type] = {}; }
-      sensors[s.type.type][s.name] = s._id;
-    });
-
-    const items: ICardMenuItem[] = [];
-    Object.keys(sensors).forEach(item => {
-      const subItems = [];
-
-      Object.keys(sensors[item]).forEach(subItem => {
-        subItems.push({ label: subItem, _id: sensors[item][subItem] });
+      sensorsFiltered.forEach(s => {
+        if (!sensors[s.type.type]) { sensors[s.type.type] = {}; }
+        sensors[s.type.type][s.name] = s._id;
       });
 
-      items.push({ label: item, items: arrayUtils.orderBy(subItems, 'ASC', 'label') });
-    });
+      const items: ICardMenuItem[] = [];
+      Object.keys(sensors).forEach(item => {
+        const subItems = [];
 
-    return arrayUtils.orderBy(items, 'ASC', 'label');
+        Object.keys(sensors[item]).forEach(subItem => {
+          subItems.push({ label: subItem, _id: sensors[item][subItem] });
+        });
+
+        items.push({ label: item, items: arrayUtils.orderBy(subItems, 'ASC', 'label') });
+      });
+
+      this.sensorsMenuItems.next(arrayUtils.orderBy(items, 'ASC', 'label'));
+    });
   }
 
   public isAdmin(): boolean {
@@ -93,15 +110,22 @@ export class ThingDetailsComponent implements OnInit, OnDestroy {
     return this.project.admin._id === this.authService.getTokenData().userId;
   }
 
-  public addSensor() {
+  public addSensor(): void {
     this.router.navigate([`project/${this.project._id}/thing/${this.thing._id}/sensor/create`]);
   }
 
-  public editSensor(id) {
+  public editSensor(id): void {
     this.router.navigate([`project/${this.project._id}/thing/${this.thing._id}/sensor/edit/${id}`]);
   }
 
-  ngOnDestroy() {
+  public filterSensors(filter: string): void {
+    const fields = ['name', 'type.type'];
+    this.sensorsFiltered$.next(arrayUtils.filter(this.sensors, filter, fields));
+  }
+
+  ngOnDestroy(): void {
     this.socketIOService.leave(`thing:${this.getThingId()}`);
+    this.onDestroy.next();
+    this.onDestroy.complete();
   }
 }
